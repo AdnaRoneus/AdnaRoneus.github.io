154d1d827b599e540c5b9699cefa7ef726b32aef
AdnaRoneus <47867619+AdnaRoneus@users.noreply.github.com>
2019-05-22 07:29:39 -0300

    Add files via upload

---
+++ HumaruShooter/offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1558520750,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/starshipcontrol-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/uparrowc-sheet0.png",
+		"images/uparrowc-sheet1.png",
+		"images/downarrowc-sheet0.png",
+		"images/downarrowc-sheet1.png",
+		"images/leftarrowc-sheet0.png",
+		"images/leftarrowc-sheet1.png",
+		"images/rightarrowc-sheet0.png",
+		"images/rightarrowc-sheet1.png",
+		"images/shootbutton-sheet0.png",
+		"images/shootbutton-sheet1.png",
+		"images/tiledbackground.png",
+		"images/inimigoboi-sheet0.png",
+		"images/gravidade-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
