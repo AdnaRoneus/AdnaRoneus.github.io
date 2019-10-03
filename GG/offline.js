64f69d699bc9f5375392cf4d2a780060297366ed
AdnaRoneus <47867619+AdnaRoneus@users.noreply.github.com>
2019-10-03 10:35:46 -0300

    Add files via upload

---
+++ GG/offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1570109568,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/setau-sheet0.png",
+		"images/setau-sheet1.png",
+		"images/setal-sheet0.png",
+		"images/setal-sheet1.png",
+		"images/setar-sheet0.png",
+		"images/setar-sheet1.png",
+		"images/ataque-sheet0.png",
+		"images/ataque-sheet1.png",
+		"images/colisões.png",
+		"images/personagemimagem-sheet0.png",
+		"images/personagemhitbox-sheet0.png",
+		"images/personagembala-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
